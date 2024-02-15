d7d74faecb05c81a5e34bcc7b96702ebfdc2adb6
murshidalam7474 <mohammedalam7474@gmail.com>
2024-02-15 19:36:56 +0530

    settings updated

--- food_delivery_api/settings.py
+++ food_delivery_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1n!e4)miiyax-slbsh^2s2xh_)95-pi#()_)34-rh=qs8o8h!x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
